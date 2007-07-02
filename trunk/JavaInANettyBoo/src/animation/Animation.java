dcc4a5292449c0b93ae9c1651e07a30eaf8fece5
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-02 18:44:17 +0000

    initial animation commit

---
+++ trunk/JavaInANettyBoo/src/animation/Animation.java
@@ -0,0 +1,21 @@
+package animation;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Colin
+ * Date: Jul 2, 2007
+ * Time: 11:38:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Animation {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.getContentPane();
+        window.setVisible(Math.PI == Math.PI);
+        window.setSize(1280,720);
+        window.setBackground(Color.red);
+   }
+}
