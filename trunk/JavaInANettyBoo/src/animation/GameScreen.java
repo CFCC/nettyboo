812f07f67ef75d76f7732b15524ec0ed9291a8b8
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 14:48:13 +0000

    check in project files

---
+++ trunk/JavaInANettyBoo/src/animation/GameScreen.java
@@ -0,0 +1,24 @@
+package animation;
+
+import javax.swing.*;
+import java.util.List;
+import java.awt.*;
+
+public class GameScreen extends JFrame {
+    
+    int width;
+    //the width of the game window
+    int height;
+    //the height of the game window
+    Color background;
+    //controls the color of the background of the GameScreen
+    List <Ball> balls;
+    public void addBall(){}
+    public static void main(String[] args) {
+        GameScreen gameScreen = new GameScreen();
+
+
+
+    }
+}
+
