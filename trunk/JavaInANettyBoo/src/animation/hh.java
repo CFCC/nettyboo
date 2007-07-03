e3c43067ac240f34bf658423e1ef31977422a55b
charlie <charlie@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 15:00:13 +0000

    add hh

---
+++ trunk/JavaInANettyBoo/src/animation/hh.java
@@ -0,0 +1,11 @@
+package animation;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: $Billybobjoebobstinkharrisphilthe2ndofirelandandalsothepharohofegyptandhighwarlordofsomewhereelse
+ * Date: Jul 3, 2007
+ * Time: 10:57:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class hh {
+}
