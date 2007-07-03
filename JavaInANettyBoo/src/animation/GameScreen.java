152b5f5ed597ada2b5cb068cf212d42b3bfab5be
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 15:45:03 +0000

    delegte readme

--- JavaInANettyBoo/src/animation/GameScreen.java
+++ JavaInANettyBoo/src/animation/GameScreen.java
@@ -13,16 +13,37 @@ public class GameScreen extends JFrame {
     public Color background;
     //controls the color of the background of the GameScreen
     public List <Ball> balls;
+
+    public JPanel screen = new JPanel() {
+        protected void paintComponent(Graphics gg) {
+            super.paintComponent(gg);
+
+            Graphics2D g = (Graphics2D) gg;
+
+            // draw the screen
+            g.setColor(Color.green);
+            g.setStroke(new BasicStroke(30));
+            g.drawOval(400,500,250,200);
+            g.setColor(Color.blue);
+            g.drawOval(350,200,200,250);
+            g.setColor(Color.blue);
+            g.fillOval(500,400,250,200);
+            g.fillOval(500,400,200,250);
+        }
+    };
+
+    public GameScreen() {
+        add(screen);
+    }
+
     //the list of balls on screen, each ball is assigned a specific number
     public void addBall(){}
     public static void main(String[] args) {
         GameScreen gameScreen = new GameScreen();
-        gameScreen.setVisible(true);
         gameScreen.setSize(1280,800);
-        gameScreen.setBackground(Color.black);
-
-
-
+        gameScreen.setVisible(true);
+        gameScreen.screen.setBackground(Color.black);
     }
+
 }
 
