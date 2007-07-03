d9f0c4d8de90782262329be4d489aee8e8eb2092
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 14:48:13 +0000

    check in project files

---
+++ JavaInANettyBoo/src/animation/Ball.java
@@ -0,0 +1,15 @@
+package animation;
+
+import java.awt.*;
+
+public class Ball {
+    Point coordinate;
+    //the coordinate(s) that the ball occupies
+    Point speed;
+    //how fast the ball moves relative to how fast the ball's points change
+    Color color;
+    //the color of the ball
+    int radius;
+    //the radius(half diameter) of the ball
+
+}
