7f5255f887d4d39f33a76652d208f1db829da8b1
tom <tom@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 14:57:02 +0000

    make fields public

--- trunk/JavaInANettyBoo/src/animation/GameScreen.java
+++ trunk/JavaInANettyBoo/src/animation/GameScreen.java
@@ -6,13 +6,13 @@ import java.awt.*;
 
 public class GameScreen extends JFrame {
     
-    int width;
+    public int width;
     //the width of the game window
-    int height;
+    public int height;
     //the height of the game window
-    Color background;
+    public Color background;
     //controls the color of the background of the GameScreen
-    List <Ball> balls;
+    public List <Ball> balls;
     public void addBall(){}
     public static void main(String[] args) {
         GameScreen gameScreen = new GameScreen();
