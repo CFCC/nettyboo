684a6e12ef17518fc32e36f0d3cd5f57f0fa014f
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-03 14:58:07 +0000

    check in project files

--- JavaInANettyBoo/src/animation/GameScreen.java
+++ JavaInANettyBoo/src/animation/GameScreen.java
@@ -13,9 +13,13 @@ public class GameScreen extends JFrame {
     public Color background;
     //controls the color of the background of the GameScreen
     public List <Ball> balls;
+    //the list of balls on screen, each ball is assigned a specific number
     public void addBall(){}
     public static void main(String[] args) {
         GameScreen gameScreen = new GameScreen();
+        gameScreen.setVisible(true);
+        gameScreen.setSize(1280,800);
+        gameScreen.setBackground(Color.black);
 
 
 
