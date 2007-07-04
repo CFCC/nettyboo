6bd6de83aa90f2947d5c3594c4506568c25267e7
colin <colin@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-04 14:12:26 +0000

    commit changes to game screen

--- JavaInANettyBoo/src/animation/GameScreen.java
+++ JavaInANettyBoo/src/animation/GameScreen.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GameScreen extends JFrame {
     
@@ -18,25 +20,29 @@ public class GameScreen extends JFrame {
         return balls;
     }
 
-    public JPanel screen;
-
-    {
-        screen = new JPanel() {
-            protected void paintComponent(Graphics gg) {
-                super.paintComponent(gg);
-
-                Graphics2D g = (Graphics2D) gg;
-
+    public JPanel screen = new JPanel() {
+        protected void paintComponent(Graphics gg) {
+            super.paintComponent(gg);
+            Graphics2D g = (Graphics2D) gg;
+            for (Ball fart : balls) {
+                g.fillOval(fart.getPosition().x,fart.getPosition().y,60,60);
             }
-        };
-    }
-
+        }
+    };
     public GameScreen() {
+        new Timer(1000/30, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+            }
+        }).start();
         add(screen);
     }
 
     //the list of balls on screen, each ball is assigned a specific number
-    public void addBall(Ball ball){}
+    public void addBall(Ball fart){
+        balls.add(fart);
+    }
+
     public static void main(String[] args) {
         GameScreen gameScreen = new GameScreen();
         gameScreen.setSize(1280,800);
