ac4cac1f412fd6ee3daa9183ee135a2ba9c1cdcf
keith <keith@f78d4240-1e40-0410-9ca2-6f40c8c0eed9>
2007-07-06 15:54:25 +0000

    Show "click and drag"

--- JavaInANettyBoo/src/animation/N00bPwner.java
+++ JavaInANettyBoo/src/animation/N00bPwner.java
@@ -3,25 +3,56 @@ package animation;
 import java.awt.Graphics2D;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Point;
+import java.awt.BasicStroke;
 import java.awt.font.TextLayout;
+import java.util.List;
 
 public class N00bPwner {
     private static final String START_PLAYING_STRING = "Right-click to play";
 
     private final GameScreen gameScreen;
+    private static final Color DARK_GRAY = new Color(30, 30, 30);
 
     public N00bPwner(GameScreen gameScreen) {
         this.gameScreen = gameScreen;
     }
 
     public void pwn(Graphics2D g) {
-        if (gameScreen.getScreenObjects().isEmpty()) {
+        List<ScreenObject> objects = gameScreen.getScreenObjects();
+        if (objects.isEmpty()) {
             Font font = findLargestFont(g, START_PLAYING_STRING, (int) (gameScreen.screen.getWidth() * .9));
-            g.setColor(new Color(30, 30, 30));
+            g.setColor(DARK_GRAY);
             g.setFont(font);
             g.drawString(START_PLAYING_STRING,
                     (float) (gameScreen.screen.getWidth() * .05), gameScreen.screen.getHeight()/2);
+        } else {
+            List<Ball> balls = gameScreen.getBalls();
+            for (Ball object : balls) {
+                if (object.getSpeed().distance(0, 0) == 0) {
+                    g.setColor(Color.GRAY);
+                    g.setFont(new Font("blah", Font.BOLD, 20));
+                    Point pos = object.getPosition();
+                    int radius = object.getRadius() - 5;
+                    int tx = pos.x + radius;
+                    int ty = pos.y + radius;
+                    g.drawString("Click and drag to throw", tx, ty);
+                    g.setStroke(new BasicStroke(1));
+                    g.drawLine(tx-8, ty-g.getFont().getSize()/2, pos.x, pos.y);
+                }
+            }
         }
+    }
+
+    private boolean areSomeObjectsMoving(List<? extends ScreenObject> objects) {
+        boolean someMoving = false;
+        for (ScreenObject object : objects) {
+            if (object.getSpeed().distance(0, 0) != 0) {
+                someMoving = true;
+                break;
+            }
+        }
+        return someMoving;
     }
 
     private Font findLargestFont(Graphics2D g, String string, int maxWidth) {
